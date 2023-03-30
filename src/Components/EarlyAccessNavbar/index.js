9477891f8063a5bf27a395218804a55894bdb88c
Ashish Jingar <jingarashish.57@gmail.com>
2023-03-31 00:26:10 +0530

    Early Access Who we are and Sign Up Page UI with Functionality

--- src/Components/EarlyAccessNavbar/index.js
+++ src/Components/EarlyAccessNavbar/index.js
@@ -4,51 +4,55 @@ import logo from "../../assets/skillcoup_blue.png";
 import "./navbar.css";
 
 const EarlyAccessNavbar = (props) => {
-    if(props.index === 1)
-        return (
-            // IF index is 1
-            <>
-            <div className="navbar">
-                <div className="navbar-logo">
-                    <img className="logo" src={logo} alt="logo" />
-                </div>
-                <div className="navbar-buttons">
-                    <button className="active-button">Home</button>
-                    <button className="navbar-button">Who We Are</button>
-                </div>
-            </div>
-            </>
-                )
-        if(props.index === 2)
-            return (
-                
-                // IF index is 1
-                <>
-                <div className="navbar">
-                    <div className="navbar-logo">
-                        <img className="logo" src={logo} alt="logo" />
-                    </div>
-                    <div className="navbar-buttons">
-                        <button className="navbar-button">Home</button>
-                        <button className="active-button">Who We Are</button>
-                    </div>
-                </div>
-                </>
-                    )
-        else
-            return (
-                // IF index is 1
-                <>
-                <div className="navbar">
-                    <div className="navbar-logo">
-                        <img className="logo" src={logo} alt="logo" />
-                    </div>
-                    <div className="navbar-buttons">
-                        <button className="navbar-button">Home</button>
-                        <button className="navbar-button">Who We Are</button>
-                    </div>
-                </div>
-                </>)
-}
+  if (props.index === 1)
+    return (
+      // IF index is 1
+      <>
+        <div className="navbar">
+          <div className="navbar-logo">
+            <img className="logo" src={logo} alt="logo" />
+          </div>
+          <div className="navbar-buttons">
+            <button className="active-button" href="/">
+              Home
+            </button>
+            <button className="navbar-button">
+              Who We Are
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  if (props.index === 2)
+    return (
+      // IF index is 1
+      <>
+        <div className="navbar">
+          <div className="navbar-logo">
+            <img className="logo" src={logo} alt="logo" />
+          </div>
+          <div className="navbar-buttons">
+            <button className="navbar-button">Home</button>
+            <button className="active-button">Who We Are</button>
+          </div>
+        </div>
+      </>
+    );
+  else
+    return (
+      // IF index is 1
+      <>
+        <div className="navbar">
+          <div className="navbar-logo">
+            <img className="logo" src={logo} alt="logo" />
+          </div>
+          <div className="navbar-buttons">
+            <button className="navbar-button">Home</button>
+            <button className="navbar-button">Who We Are</button>
+          </div>
+        </div>
+      </>
+    );
+};
 
 export default EarlyAccessNavbar;
